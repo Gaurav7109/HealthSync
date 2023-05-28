8580e7d26cbe325bd2576eef3def0aec7e6098a1
“gaurav” <2020uee1211@mnit.ac.in>
2023-05-29 02:01:54 +0530

    first commit
    deployment

---
+++ client/routes/userRoutes.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const {
+  loginController,
+  registerController,
+  authController,
+  applyDoctorController,
+  getAllNotificationController,
+  deleteAllNotificationController,
+  getAllDoctorsController,
+  bookAppointmentController,
+  bookingAvailabilityController,
+  userAppointmentsController,
+  userProfileController,
+} = require("../controller/userCtrl");
+const authMiddleware = require("../middleware/authMiddleware");
+
+//router onject
+const router = express.Router();
+
+//routes
+//LOGIN || POST
+router.post("/login", loginController);
+
+//REGISTER || POST
+router.post("/register", registerController);
+
+//Apply Doctor || POST
+
+router.post("/apply-doctor",authMiddleware, applyDoctorController);
+
+// AUTH || POST
+router.post('/getUserData',authMiddleware,authController);
+module.exports = router;
+
+// Get Notifications || POST
+router.post('/get-all-notification',authMiddleware,getAllNotificationController);
+
+// Delete Notification || POST
+router.post('/delete-all-notification',authMiddleware,deleteAllNotificationController);
+
+//GET ALL DOC
+router.get("/getAllDoctors", authMiddleware, getAllDoctorsController);
+
+//BOOK APPOINTMENT
+router.post("/book-appointment", authMiddleware, bookAppointmentController);
+
+//Booking Availability
+router.post(
+  "/booking-availability",
+  authMiddleware,
+  bookingAvailabilityController
+);
+
+//Appointments List
+router.get("/user-appointments", authMiddleware, userAppointmentsController);
+
+
+module.exports = router;
---
+++ client/server.js
@@ -0,0 +1,49 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const colors = require("colors");
+const moragan = require("morgan");
+
+const connectDB = require("./config/db");
+const path = require('path');
+
+//dotenv conig
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(moragan("dev"));
+
+//routes
+app.use("/api/v1/users", require("./routes/userRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/doctor",require("./routes/doctorRoutes"));
+// JUST FOR TESTING PURPOSE
+
+// app.get("/",(req,res)=>{
+//     res.status(200).send({
+//         message: "server running"
+//     });
+// });
+
+// static files for production
+app.use(express.static(path.join(__dirname,'./client/build')));
+
+app.get('*',function(req,res){
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+//port
+const port = process.env.PORT || 4080;
+//listen port
+app.listen(port, () => {
+  console.log(
+    `Server Running in ${process.env.DEV_MODE} Mode on port ${process.env.PORT}`
+      .bgCyan.white
+  );
+});
