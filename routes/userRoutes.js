9dbb7ff325c117188a1bfe605cd44d8ca5dc284b
“gaurav” <2020uee1211@mnit.ac.in>
2023-06-23 13:36:50 +0530

    resolved some bugs

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,58 +1,20 @@
 const express = require("express");
 const {
-  loginController,
+  getAllUsers,
   registerController,
-  authController,
-  applyDoctorController,
-  getAllNotificationController,
-  deleteAllNotificationController,
-  getAllDoctorsController,
-  bookAppointmentController,
-  bookingAvailabilityController,
-  userAppointmentsController,
-  userProfileController,
-} = require("../controller/userCtrl");
-const authMiddleware = require("../middleware/authMiddleware");
+  loginController,
+} = require("../controllers/userContoller");
 
-//router onject
+//router object
 const router = express.Router();
 
-//routes
-//LOGIN || POST
-router.post("/login", loginController);
+// GET ALL USERS || GET
+router.get("/all-users", getAllUsers);
 
-//REGISTER || POST
+// CREATE USER || POST
 router.post("/register", registerController);
 
-//Apply Doctor || POST
-
-router.post("/apply-doctor",authMiddleware, applyDoctorController);
-
-// AUTH || POST
-router.post('/getUserData',authMiddleware,authController);
-// module.exports = router;
-
-// Get Notifications || POST
-router.post('/get-all-notification',authMiddleware,getAllNotificationController);
-
-// Delete Notification || POST
-router.post('/delete-all-notification',authMiddleware,deleteAllNotificationController);
-
-//GET ALL DOC
-router.get("/getAllDoctors", authMiddleware, getAllDoctorsController);
-
-//BOOK APPOINTMENT
-router.post("/book-appointment", authMiddleware, bookAppointmentController);
-
-//Booking Availability
-router.post(
-  "/booking-availability",
-  authMiddleware,
-  bookingAvailabilityController
-);
-
-//Appointments List
-router.get("/user-appointments", authMiddleware, userAppointmentsController);
-
+//LOGIN || POST
+router.post("/login", loginController);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,49 +1,46 @@
-const dotenv = require("dotenv");
 const express = require("express");
-const colors = require("colors");
+const cors = require("cors");
 const morgan = require("morgan");
-
+const colors = require("colors");
+const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const path = require('path');
+const path = require("path");
 
-//dotenv conig
+//env config
 dotenv.config();
+
+//router import
+const userRoutes = require("./routes/userRoutes");
+const blogRoutes = require("./routes/blogRoutes");
 
 //mongodb connection
 connectDB();
 
-//rest object
+//rest objecct
 const app = express();
 
-//middlewares
+//middelwares
+app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 
 //routes
-app.use("/api/v1/users", require("./routes/userRoutes"));
-app.use("/api/v1/admin", require("./routes/adminRoutes"));
-app.use("/api/v1/doctor",require("./routes/doctorRoutes"));
-// JUST FOR TESTING PURPOSE
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/blog", blogRoutes);
 
-// app.get("/",(req,res)=>{
-//     res.status(200).send({
-//         message: "server running"
-//     });
-// });
-
-// static files for production
-app.use(express.static(path.join(__dirname,'./client/build')));
+// static files
+app.use(express.static(path.join(__dirname,"./client/build")));
 
 app.get('*',function(req,res){
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+})
 
-//port
-const port = process.env.PORT || 4080;
-//listen port
-app.listen(port, () => {
-  // console.log(
-  //   `Server Running in ${process.env.DEV_MODE} Mode on port ${process.env.PORT}`
-  //     .bgCyan.white
-  // );
+// Port
+const PORT = process.env.PORT || 8080;
+//listen
+app.listen(PORT, () => {
+  console.log(
+    // `Server Running on ${process.env.DEV_MODE} mode port no ${PORT}`.bgCyan
+      // .white
+  );
 });
